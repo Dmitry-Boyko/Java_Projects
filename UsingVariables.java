cca71fcae0957093c5dad3b80e4a13a89d4e77a3
boyd <8184160900.cell@gmail.com>
2016-12-05 07:15:13 +0000

    tt

---
+++ UsingVariables.java
@@ -0,0 +1,17 @@
+public class UsingVariables {
+    public static void main(String[] args){
+    
+        int testInt = 1111;
+        double testDouble = 4.788;
+        float testFloat = 34.356432224F;
+        char testChar = '#';
+        boolean testBool = true;
+    
+        System.out.println(testInt);
+        System.out.println(testDouble);
+        System.out.println(testFloat);
+        System.out.println(testChar);
+        System.out.println(testBool);
+    }
+    
+}
